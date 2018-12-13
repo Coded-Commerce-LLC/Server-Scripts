5ba275db7a8c76f53f777660b24a6a86afa4be6b
Sean Conklin <seanconklin@yahoo.com>
2018-12-12 18:45:19 -0800

    Update to MariaDB installation

--- install-lemp.sh
+++ install-lemp.sh
@@ -14,6 +14,7 @@ sudo apt install mariadb-server mariadb-client -y;
 sudo systemctl start mysql;
 sudo systemctl enable mysql;
 sudo mysql_secure_installation;
+mysql -u root mysql -e "update user set plugin='mysql_native_password' where user='root';"
 
 # Install PHP
 sudo apt install php7.2-fpm php7.2-mbstring php7.2-xml php7.2-mysql php7.2-common php7.2-gd php7.2-json php7.2-cli php7.2-curl php7.2-soap -y;
