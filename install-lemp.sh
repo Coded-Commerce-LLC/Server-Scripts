a4be21c8daebbee08807da1c9eccd2c7959e322e
Sean Conklin <sean@codedcommerce.com>
2019-04-15 22:52:52 -0700

    Added WP-CLI instructions

--- install-lemp.sh
+++ install-lemp.sh
@@ -55,3 +55,9 @@ sudo ufw allow http;
 sudo ufw allow https;
 sudo ufw enable;
 sudo ufw status verbose;
+
+# Install WP-CLI
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+chmod +x wp-cli.phar;
+sudo mv wp-cli.phar /usr/local/bin/wp;
+
